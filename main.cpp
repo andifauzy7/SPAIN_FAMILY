702fc9b12eb7a2ae19db1e4134144f58b2961fb2
andifauzy7 <andifauzy00@gmail.com>
2019-05-18 09:17:58 +0700

    Perbaiki koneksi dari setiap anak

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main(){
 	nbTree MyList;
-	nbAddr awal;
+	nbAddr awal, anak1,anak2,anak3;
 	nbCreate(&MyList);
 
 	awal=(nbAddr)malloc(sizeof(ElmtTree));
@@ -15,45 +15,37 @@ int main(){
 	awal->nb=NULL;
 	awal->fs=NULL;
 
-    awal=NULL;
-    free(awal);
+    anak1=(nbAddr)malloc(sizeof(ElmtTree));
+	anak1->nama="Fahmi Widianto";
+	anak1->jeniskelamin='L';
+	anak1->usia=25;
+	anak1->status=true;
+	anak1->parent=awal;
+	anak1->nb=NULL;
+	anak1->fs=NULL;
+	awal->fs=anak1;
 
-    awal=(nbAddr)malloc(sizeof(ElmtTree));
-	MyList.root->nb=awal;
-	awal->nama="Fahmi Widianto";
-	awal->jeniskelamin='L';
-	awal->usia=25;
-	awal->status=true;
-	awal->parent=MyList.root;
-	awal->nb=NULL;
-	awal->fs=NULL;
-	MyList.root->fs=awal;
-	awal=NULL;
-	free(awal);
 
-	awal=(nbAddr)malloc(sizeof(ElmtTree));
-	MyList.root->nb=awal;
-	awal->nama="Febriyoga Bagus";
-	awal->jeniskelamin='L';
-	awal->usia=28;
-	awal->status=false;
-	awal->parent=MyList.root;
-	awal->nb=NULL;
-	awal->fs=NULL;
-	MyList.root->fs->nb=awal;
-	awal=NULL;
-	free(awal);
+	anak2=(nbAddr)malloc(sizeof(ElmtTree));
+	anak2->nama="Febriyoga Bagus";
+	anak2->jeniskelamin='L';
+	anak2->usia=28;
+	anak2->status=false;
+	anak2->parent=awal;
+	anak2->nb=NULL;
+	anak2->fs=NULL;
+	anak1->nb=anak2;
 
-    awal=(nbAddr)malloc(sizeof(ElmtTree));
-	MyList.root->nb=awal;
-	awal->nama="Luthfi Alri";
-	awal->jeniskelamin='L';
-	awal->usia=18;
-	awal->status=true;
-	awal->parent=MyList.root;
-	awal->nb=NULL;
-	awal->fs=NULL;
-	MyList.root->fs->nb->nb=awal;
+    anak3=(nbAddr)malloc(sizeof(ElmtTree));
+	anak3->nama="Luthfi Alri";
+	anak3->jeniskelamin='L';
+	anak3->usia=18;
+	anak3->status=true;
+	anak3->parent=awal;
+	anak3->nb=NULL;
+	anak3->fs=NULL;
+	anak2->nb=anak3;
+
 	awal=NULL;
 	free(awal);
 
@@ -84,6 +76,7 @@ int main(){
         printf("Status    : %d\n",awal->status);
         printf("Orang Tua : %s\n\n",awal->parent->nama);
 
+        nbPrint(MyList.root," ");
 
 	return 0;
 }
