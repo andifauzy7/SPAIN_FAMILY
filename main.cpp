978e95e72b629c3e73f3cf471775fc398b9357b9
andifauzy7 <andifauzy00@gmail.com>
2019-05-18 11:23:37 +0700

    Siap Merge dengan Master

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include "nbtree_d.h"
 
 int main(){
+    nbAddr tempik, parent;
+    nbType Nama;
+    char JK;
+    int Age;
+    boolean St;
 
+
+	nbTree MyList;
+	nbCreate(&MyList);
+
+    tempik=nbCNode("Fahmi",'L',72,false);
+    printf("Nama : %s ,JK : %c , Usia : %d ,Status : %d \nKet: 1=Life 0=Death.", tempik->nama, tempik->jeniskelamin, tempik->usia, tempik->status);
+    printf("\n");
+
+    printf("NAMA : ");
+    scanf("%s", &Nama);
+    printf("\nJenis Kelamin : ");
+    scanf(" %c", &JK);
+    printf("\nUsia : ");
+    scanf(" %d", &Age);
+    printf("\nStatus : ");
+    scanf(" %d", &St);
+
+    Insertnode(&MyList, parent, Nama, JK, Age, St);
+
+	return 0;
 	return 0;
 }
